43c55249fbbbc8f5b865aec66c9a7a332bae7fe5
Hrenaki <shikhovwtr@mail.ru>
2022-01-03 17:10:28 +0700

    Added Vector4f and Vector4

--- SharpDXtest/MathCore.cs
+++ SharpDXtest/MathCore.cs
@@ -740,6 +740,346 @@ namespace SharpDXtest
             return "(" + x.ToString(format) + ", " + y.ToString(format) + ", " + z.ToString(format) + ")";
         }
     }
+    public struct Vector4f
+    {
+        public static Vector4f zero { get { return new Vector4f(); } }
+
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+        public float w{ get; set; }
+        public Vector4f(float x = 0, float y = 0, float z = 0, float w = 0)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+        public Vector4f(Vector2f vec, float z = 0, float w = 0)
+        {
+            x = vec.x;
+            y = vec.y;
+            this.z = z;
+            this.w = w;
+        }
+        public Vector4f(Vector3f vec, float w = 0)
+        {
+            x = vec.x;
+            y = vec.y;
+            z = vec.z;
+            this.w = w;
+        }
+        public Vector4f(params float[] values)
+        {
+            if (values.Length != 4)
+                throw new Exception("Array length must be 4.");
+
+            x = values[0];
+            y = values[1];
+            z = values[2];
+            w = values[3];
+        }
+
+        public static implicit operator Vector4(Vector4f vec) => new Vector4(vec.x, vec.y, vec.z);
+        public static explicit operator Vector4f(Vector4 vec) => new Vector4f((float)vec.x, (float)vec.y, (float)vec.z, (float)vec.w);
+
+        /// <summary>
+        /// Magnitude of vector. Equals to length()
+        /// </summary>
+        public float magnitude()
+        {
+            return (float)Math.Sqrt(squaredMagnitude());
+        }
+        /// <summary>
+        /// Magnitude of vector without root. Equals to squaredLength()
+        /// </summary>
+        public float squaredMagnitude()
+        {
+            return dotMul(this);
+        }
+        /// <summary>
+        /// Length of vector. Equals to magnitude()
+        /// </summary>
+        public float length()
+        {
+            return magnitude();
+        }
+        /// <summary>
+        /// Length of vector without root. Equals to squaredMagnitude()
+        /// </summary>
+        public float squaredLength()
+        {
+            return squaredMagnitude();
+        }
+        /// <summary>
+        /// Checks if vector small enough to be considered a zero vector
+        /// </summary>
+        public bool isZero()
+        {
+            return squaredMagnitude() < Constants.SqrFloatEpsilon;
+        }
+        public static Vector4f operator +(Vector4f v1, Vector4f v2)
+        {
+            return new Vector4f(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w);
+        }
+        public static Vector4f operator -(Vector4f v1, Vector4f v2)
+        {
+            return new Vector4f(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z, v1.w - v2.w);
+        }
+        public static Vector4f operator *(Vector4f vec, float value)
+        {
+            return new Vector4f(vec.x * value, vec.y * value, vec.z * value, vec.w * value);
+        }
+        public static Vector4f operator *(float value, Vector4f vec)
+        {
+            return new Vector4f(vec.x * value, vec.y * value, vec.z * value, vec.w * value);
+        }
+        public static Vector4f operator /(Vector4f vec, float value)
+        {
+            return new Vector4f(vec.x / value, vec.y / value, vec.z / value, vec.w / value);
+        }
+        /// <summary>
+        /// Scalar multiplication
+        /// </summary>
+        public float dotMul(Vector4f vec)
+        {
+            return x * vec.x + y * vec.y + z * vec.z + w * vec.w;
+        }
+        /// <summary>
+        /// Scalar multiplication
+        /// </summary>
+        public static float operator *(Vector4f v1, Vector4f v2)
+        {
+            return v1.dotMul(v2);
+        }
+        /// <summary>
+        /// Component multiplication
+        /// </summary>
+        /// <returns>New vector - (x1*x2, y1*y2, z1*z2, w1*w2)</returns>
+        public Vector4f compMul(Vector4f vec)
+        {
+            return new Vector4f(x * vec.x, y * vec.y, z * vec.z, w * vec.w);
+        }
+        /// <summary>
+        /// Returns normalized copy of this vector
+        /// </summary>
+        public Vector4f normalized()
+        {
+            return this / magnitude();
+        }
+        /// <summary>
+        /// Normalizes this vector
+        /// </summary>
+        public void normalize()
+        {
+            float magn = magnitude();
+            x /= magn;
+            y /= magn;
+            z /= magn;
+            w /= magn;
+        }
+        /// <summary>
+        /// Checks if vectors same enough to be considered equal
+        /// </summary>
+        public bool equals(Vector4f vec)
+        {
+            return (vec - this).isZero();
+        }
+        /// <summary>
+        /// Projects vector on another vector
+        /// </summary>
+        public Vector4f projectOnVector(Vector4f vec)
+        {
+            if (vec.isZero())
+                return Vector4f.zero;
+            return vec * (this * vec / vec.squaredMagnitude());
+        }
+        /// <summary>
+        /// Projects vector on flat
+        /// </summary>
+        /// <param name="flatNorm">Normal vector to flat (not necessary normalized)</param>
+        /// <returns></returns>
+        public Vector4f projectOnFlat(Vector4f flatNorm)
+        {
+            return this - flatNorm * (this * flatNorm / flatNorm.squaredMagnitude());
+        }
+        public override string ToString()
+        {
+            return "(" + x.ToString() + ", " + y.ToString() + ", " + z.ToString() + ")";
+        }
+        public string ToString(string format)
+        {
+            return "(" + x.ToString(format) + ", " + y.ToString(format) + ", " + z.ToString(format) + ")";
+        }
+    }
+    public struct Vector4
+    {
+        public static Vector4 zero { get { return new Vector4(); } }
+
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+        public double w { get; set; }
+        public Vector4(double x = 0, double y = 0, double z = 0, double w = 0)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+        public Vector4(Vector2 vec, double z = 0, double w = 0)
+        {
+            x = vec.x;
+            y = vec.y;
+            this.z = z;
+            this.w = w;
+        }
+        public Vector4(Vector3 vec, double w = 0)
+        {
+            x = vec.x;
+            y = vec.y;
+            z = vec.z;
+            this.w = w;
+        }
+        public Vector4(params double[] values)
+        {
+            if (values.Length != 4)
+                throw new Exception("Array length must be 4.");
+
+            x = values[0];
+            y = values[1];
+            z = values[2];
+            w = values[3];
+        }
+        /// <summary>
+        /// Magnitude of vector. Equals to length()
+        /// </summary>
+        public double magnitude()
+        {
+            return Math.Sqrt(squaredMagnitude());
+        }
+        /// <summary>
+        /// Magnitude of vector without root. Equals to squaredLength()
+        /// </summary>
+        public double squaredMagnitude()
+        {
+            return dotMul(this);
+        }
+        /// <summary>
+        /// Length of vector. Equals to magnitude()
+        /// </summary>
+        public double length()
+        {
+            return magnitude();
+        }
+        /// <summary>
+        /// Length of vector without root. Equals to squaredMagnitude()
+        /// </summary>
+        public double squaredLength()
+        {
+            return squaredMagnitude();
+        }
+        /// <summary>
+        /// Checks if vector small enough to be considered a zero vector
+        /// </summary>
+        public bool isZero()
+        {
+            return squaredMagnitude() < Constants.SqrEpsilon;
+        }
+        public static Vector4 operator +(Vector4 v1, Vector4 v2)
+        {
+            return new Vector4(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w);
+        }
+        public static Vector4 operator -(Vector4 v1, Vector4 v2)
+        {
+            return new Vector4(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z, v1.w - v2.w);
+        }
+        public static Vector4 operator *(Vector4 vec, double value)
+        {
+            return new Vector4(vec.x * value, vec.y * value, vec.z * value, vec.w * value);
+        }
+        public static Vector4 operator *(double value, Vector4 vec)
+        {
+            return new Vector4(vec.x * value, vec.y * value, vec.z * value, vec.w * value);
+        }
+        public static Vector4 operator /(Vector4 vec, double value)
+        {
+            return new Vector4(vec.x / value, vec.y / value, vec.z / value, vec.w / value);
+        }
+        /// <summary>
+        /// Scalar multiplication
+        /// </summary>
+        public double dotMul(Vector4 vec)
+        {
+            return x * vec.x + y * vec.y + z * vec.z + w * vec.w;
+        }
+        /// <summary>
+        /// Scalar multiplication
+        /// </summary>
+        public static double operator *(Vector4 v1, Vector4 v2)
+        {
+            return v1.dotMul(v2);
+        }
+        /// <summary>
+        /// Component multiplication
+        /// </summary>
+        /// <returns>New vector - (x1*x2, y1*y2, z1*z2, w1*w2)</returns>
+        public Vector4 compMul(Vector4 vec)
+        {
+            return new Vector4(x * vec.x, y * vec.y, z * vec.z, w * vec.w);
+        }
+        /// <summary>
+        /// Returns normalized copy of this vector
+        /// </summary>
+        public Vector4 normalized()
+        {
+            return this / magnitude();
+        }
+        /// <summary>
+        /// Normalizes this vector
+        /// </summary>
+        public void normalize()
+        {
+            double magn = magnitude();
+            x /= magn;
+            y /= magn;
+            z /= magn;
+            w /= magn;
+        }
+        /// <summary>
+        /// Checks if vectors same enough to be considered equal
+        /// </summary>
+        public bool equals(Vector4 vec)
+        {
+            return (vec - this).isZero();
+        }
+        /// <summary>
+        /// Projects vector on another vector
+        /// </summary>
+        public Vector4 projectOnVector(Vector4 vec)
+        {
+            if (vec.isZero())
+                return Vector4.zero;
+            return vec * (this * vec / vec.squaredMagnitude());
+        }
+        /// <summary>
+        /// Projects vector on flat
+        /// </summary>
+        /// <param name="flatNorm">Normal vector to flat (not necessary normalized)</param>
+        /// <returns></returns>
+        public Vector4 projectOnFlat(Vector4 flatNorm)
+        {
+            return this - flatNorm * (this * flatNorm / flatNorm.squaredMagnitude());
+        }
+        public override string ToString()
+        {
+            return "(" + x.ToString() + ", " + y.ToString() + ", " + z.ToString() + ")";
+        }
+        public string ToString(string format)
+        {
+            return "(" + x.ToString(format) + ", " + y.ToString(format) + ", " + z.ToString(format) + ")";
+        }
+    }
     public struct Matrix4x4f
     {
         public float v00 { get; set; } // [rowIndex, columnIndex]
