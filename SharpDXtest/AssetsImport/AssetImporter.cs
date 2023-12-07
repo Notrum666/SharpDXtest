da0d2f58d196e9c26714a0b4be14d1bf7f049240
Yaroslav Boyev <yaroslav.r.boyev@gmail.com>
2023-12-07 21:16:19 +0300

    Add AssetsRegistry

---
+++ SharpDXtest/AssetsImport/AssetImporter.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+using Engine;
+using Engine.AssetsData;
+
+namespace Editor.AssetsImport
+{
+    public abstract class AssetImporter
+    {
+        [YamlTagMapped]
+        public class BaseImportSettings { }
+        
+        protected abstract BaseImportSettings GetDefaultSettings();
+
+        protected abstract AssetData OnImportAsset(string assetPath, AssetMeta assetMeta);
+
+        public AssetMeta ImportAsset(string assetSourcePath)
+        {
+            ValidateExtension(assetSourcePath);
+            string metaPath = GetMetaPath(assetSourcePath);
+            string contentRelativePath = AssetsRegistry.GetContentAssetPath(assetSourcePath);
+
+            AssetMeta assetMeta = LoadMetaFile(metaPath);
+
+            bool metaOutOfDate = assetMeta.ImportDate < File.GetLastWriteTimeUtc(assetSourcePath);
+            bool artifactOutOfDate = AssetsManager.GetAssetImportDate(contentRelativePath) < assetMeta.ImportDate;
+
+            Logger.Log(LogType.Error, $"metaOutOfDate = {metaOutOfDate} | artifactOutOfDate = {artifactOutOfDate}");
+
+            if (metaOutOfDate || artifactOutOfDate)
+            {
+                AssetData importedAsset = OnImportAsset(assetSourcePath, assetMeta);
+                if (importedAsset == null)
+                    return null;
+
+                if (metaOutOfDate)
+                    SaveMetaFile(metaPath, assetMeta);
+
+                AssetsManager.SaveAsset(contentRelativePath, assetMeta.Guid, importedAsset);
+            }
+
+            return assetMeta;
+        }
+
+        private AssetMeta LoadMetaFile(string metaPath)
+        {
+            AssetMeta savedMeta = YamlManager.LoadFromFile<AssetMeta>(metaPath);
+            return savedMeta ?? new AssetMeta()
+            {
+                ImportSettings = GetDefaultSettings()
+            };
+        }
+
+        private void SaveMetaFile(string metaPath, AssetMeta assetMeta)
+        {
+            assetMeta.ImportDate = DateTime.UtcNow;
+            YamlManager.SaveToFile(metaPath, assetMeta);
+        }
+        
+        private string GetMetaPath(string assetSourcePath)
+        {
+            string assetExtension = Path.GetExtension(assetSourcePath);
+            return Path.ChangeExtension(assetSourcePath, $"{assetExtension}{AssetMeta.MetaExtension}");
+        }
+
+        private void ValidateExtension(string assetSourcePath)
+        {
+            string extension = Path.GetExtension(assetSourcePath);
+            IEnumerable<string> extensions = GetType().GetCustomAttribute<AssetImporterAttribute>()?.Extensions ?? Enumerable.Empty<string>();
+            Logger.Assert(extensions.Contains(extension));
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AssetImporterAttribute : Attribute
+    {
+        public IEnumerable<string> Extensions { get; }
+
+        public AssetImporterAttribute(params string[] ext)
+        {
+            Extensions = ext.Where(s => !string.IsNullOrWhiteSpace(s)).Select(x => $".{x}");
+        }
+    }
+}
---
+++ SharpDXtest/AssetsImport/AssetsRegistry.cs
@@ -0,0 +1,238 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+using Editor.AssetsImport;
+
+using Engine;
+
+using static Engine.FileSystemHelper;
+
+namespace Editor
+{
+    public static class AssetsRegistry
+    {
+        public const string ContentFolderName = "Content";
+        public static string ContentFolderPath { get; private set; }
+
+        private static readonly Dictionary<string, AssetImporter> assetImporters = new Dictionary<string, AssetImporter>();
+
+        static AssetsRegistry()
+        {
+            SetupImporters();
+        }
+
+        private static void SetupImporters()
+        {
+            Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (Type type in types)
+            {
+                AssetImporterAttribute importerAttribute = type.GetCustomAttribute<AssetImporterAttribute>();
+                if (importerAttribute != null)
+                {
+                    AssetImporter importer = Activator.CreateInstance(type) as AssetImporter;
+                    foreach (string extension in importerAttribute.Extensions)
+                    {
+                        assetImporters[extension] = importer;
+                    }
+                }
+            }
+        }
+
+        public static void InitializeInFolder(string parentFolderPath)
+        {
+            Logger.Log(LogType.Info, $"Initialize AssetRegistry in folder = {parentFolderPath}");
+
+            ContentFolderPath = Path.Combine(parentFolderPath, ContentFolderName);
+            Directory.CreateDirectory(ContentFolderPath);
+
+            Refresh();
+        }
+
+        public static void Refresh()
+        {
+            ImportFolder(ContentFolderPath);
+        }
+
+        #region AssetOperations //Import, Copy, Move, Rename, Delete
+
+        public static void ImportAsset(string assetPath)
+        {
+            if (!Path.Exists(assetPath))
+                return;
+
+            string assetExtension = Path.GetExtension(assetPath);
+            if (assetImporters.TryGetValue(assetExtension, out AssetImporter importer))
+                importer.ImportAsset(assetPath);
+        }
+
+        public static bool CopyAsset(string assetPath, string newAssetPath)
+        {
+            if (!Path.Exists(assetPath))
+                return false;
+
+            newAssetPath = GenerateUniquePath(newAssetPath);
+            File.Copy(assetPath, newAssetPath);
+            ImportAsset(newAssetPath);
+            return true;
+        }
+
+        public static bool MoveAsset(string oldAssetPath, string newAssetPath)
+        {
+            if (!Path.Exists(oldAssetPath) || Path.Exists(newAssetPath))
+                return false;
+
+            string oldContentAssetPath = GetContentAssetPath(oldAssetPath);
+            string newContentAssetPath = GetContentAssetPath(newAssetPath);
+
+            if (!IsSupportedAssetFile(oldAssetPath) || !AssetsManager.UpdateAssetPath(oldContentAssetPath, newContentAssetPath))
+                return false;
+
+            File.Move(oldAssetPath, newAssetPath);
+            return true;
+        }
+
+        public static bool RenameAsset(string assetPath, string newAssetName)
+        {
+            FileInfo fileInfo = new FileInfo(assetPath);
+
+            string newAssetPath = Path.Combine(fileInfo.DirectoryName ?? string.Empty, $"{newAssetName}{fileInfo.Extension}");
+            return MoveAsset(assetPath, newAssetPath);
+        }
+
+        public static bool DeleteAsset(string assetPath)
+        {
+            if (!Path.Exists(assetPath))
+                return false;
+
+            string contentAssetPath = GetContentAssetPath(assetPath);
+
+            if (IsSupportedAssetFile(assetPath) && !AssetsManager.DeleteAsset(contentAssetPath))
+                return false;
+
+            File.Delete(assetPath);
+            return true;
+        }
+
+        #endregion AssetOperations
+
+        #region FolderOperations //Import, Create, Copy, Move, Rename, Delete
+
+        public static void ImportFolder(string folderPath)
+        {
+            if (!Path.Exists(folderPath))
+                return;
+            Console.WriteLine($"Folder import at {folderPath}");
+
+            foreach (FileSystemHelper.PathInfo pathInfo in EnumeratePathInfoEntries(folderPath, "*", true))
+            {
+                if (!pathInfo.IsDirectory)
+                    ImportAsset(pathInfo.FullPath);
+            }
+        }
+
+        public static void CreateFolder(string parentFolderPath, string newFolderName)
+        {
+            string fullPath = Path.Combine(parentFolderPath, newFolderName);
+            fullPath = GenerateUniquePath(fullPath);
+            Directory.CreateDirectory(fullPath);
+        }
+
+        public static bool CopyFolder(string folderPath, string newFolderPath)
+        {
+            if (!Path.Exists(folderPath))
+                return false;
+
+            newFolderPath = GenerateUniquePath(newFolderPath);
+            Directory.CreateDirectory(newFolderPath);
+
+            foreach (FileSystemHelper.PathInfo pathInfo in EnumeratePathInfoEntries(folderPath, "*", true))
+            {
+                if (!pathInfo.IsDirectory)
+                    continue;
+                string relativePath = Path.GetRelativePath(folderPath, pathInfo.FullPath);
+                Directory.CreateDirectory(Path.Combine(newFolderPath, relativePath));
+            }
+
+            foreach (FileSystemHelper.PathInfo pathInfo in EnumeratePathInfoEntries(folderPath, "*", true))
+            {
+                if (pathInfo.IsDirectory || Path.GetExtension(pathInfo.FullPath) == AssetMeta.MetaExtension)
+                    continue;
+                string relativePath = Path.GetRelativePath(folderPath, pathInfo.FullPath);
+                string newAssetPath = Path.Combine(newFolderPath, relativePath);
+                CopyAsset(pathInfo.FullPath, newAssetPath);
+            }
+
+            return true;
+        }
+
+        public static bool MoveFolder(string oldFolderPath, string newFolderPath)
+        {
+            if (!Path.Exists(oldFolderPath) || Path.Exists(newFolderPath))
+                return false;
+
+            foreach (FileSystemHelper.PathInfo pathInfo in EnumeratePathInfoEntries(oldFolderPath, "*", true))
+            {
+                if (pathInfo.IsDirectory || !IsSupportedAssetFile(pathInfo.FullPath))
+                    continue;
+
+                string relativeAssetPath = Path.GetRelativePath(oldFolderPath, pathInfo.FullPath);
+                string newAssetPath = Path.Combine(newFolderPath, relativeAssetPath);
+
+                string oldContentAssetPath = GetContentAssetPath(pathInfo.FullPath);
+                string newContentAssetPath = GetContentAssetPath(newAssetPath);
+
+                if (!AssetsManager.UpdateAssetPath(oldContentAssetPath, newContentAssetPath))
+                    return false;
+            }
+
+            Directory.Move(oldFolderPath, newFolderPath);
+            return true;
+        }
+
+        public static bool RenameFolder(string folderPath, string newFolderName)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+
+            string newFolderPath = Path.Combine(directoryInfo.Parent?.FullName ?? "", newFolderName);
+            return MoveFolder(folderPath, newFolderPath);
+        }
+
+        public static bool DeleteFolder(string folderPath)
+        {
+            if (!Path.Exists(folderPath))
+                return false;
+
+            foreach (FileSystemHelper.PathInfo pathInfo in EnumeratePathInfoEntries(folderPath, "*", true))
+            {
+                if (pathInfo.IsDirectory || !IsSupportedAssetFile(pathInfo.FullPath))
+                    continue;
+
+                string contentRelativePath = GetContentAssetPath(pathInfo.FullPath);
+                if (!AssetsManager.DeleteAsset(contentRelativePath))
+                    return false;
+            }
+
+            Directory.Delete(folderPath, true);
+            return true;
+        }
+
+        #endregion FolderOperations
+
+        #region Helpers
+
+        public static bool IsSupportedAssetFile(string fullPath)
+        {
+            return assetImporters.ContainsKey(Path.GetExtension(fullPath));
+        }
+
+        public static string GetContentAssetPath(string fullPath)
+        {
+            return Path.GetRelativePath(ContentFolderPath, fullPath);
+        }
+
+        #endregion Helpers
+
+    }
+}
