cbf2910c23b85d2f9639e397fd582159198f26a6
Yaroslav Boyev <yaroslav.r.boyev@gmail.com>
2023-12-07 21:13:35 +0300

    Add FileSystemHelper

--- Engine/Utilities.cs
+++ Engine/Utilities.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.IO.Enumeration;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -13,6 +14,7 @@ using System.Xml.Linq;
 using System.Windows.Media.Imaging;
 using System.Windows;
 using System.Text;
+using System.Text.RegularExpressions;
 
 using Engine.BaseAssets.Components;
 
@@ -1936,5 +1938,80 @@ namespace Engine
             Scenes[Path.GetFileNameWithoutExtension(path)] = scene as Scene;
             return scene as Scene;
         }
+    }
+    
+    public static partial class FileSystemHelper
+    {
+        private static readonly EnumerationOptions defaultEnumerator = new EnumerationOptions() { IgnoreInaccessible = true };
+        private static readonly EnumerationOptions recursiveEnumerator = new EnumerationOptions() { IgnoreInaccessible = true, RecurseSubdirectories = true };
+
+        public static string SanitizeFileName(string name)
+        {
+            int fileNameIndex = name.LastIndexOf(Path.DirectorySeparatorChar) + 1;
+            StringBuilder path = new StringBuilder(name[..fileNameIndex]);
+            StringBuilder fileName = new StringBuilder(name[fileNameIndex..]);
+
+            foreach (char c in Path.GetInvalidPathChars())
+            {
+                path.Replace(c, '_');
+            }
+            foreach (char c in Path.GetInvalidFileNameChars())
+            {
+                fileName.Replace(c, '_');
+            }
+
+            return path.Append(fileName).ToString();
+        }
+
+        public static string GenerateUniquePath(string path)
+        {
+            if (!Path.Exists(path))
+                return path;
+
+            path = Path.TrimEndingDirectorySeparator(path);
+
+            FileInfo fileInfo = new FileInfo(path);
+            string parentFolderName = fileInfo.DirectoryName ?? string.Empty;
+            string fileName = fileInfo.Name;
+            string extension = fileInfo.Extension;
+
+            Match regexMatch = FileNameIndexRegex().Match(fileName);
+            int fileNameIndex = regexMatch.Success ? int.Parse(regexMatch.Value) : 0;
+            fileName = fileName[..^regexMatch.Length];
+
+            do
+            {
+                fileNameIndex++;
+                path = Path.Combine(parentFolderName, $"{fileName} {fileNameIndex}{extension}");
+            } while (Path.Exists(path));
+
+            return path;
+        }
+
+        public static IEnumerable<PathInfo> EnumeratePathInfoEntries(string path, string expression, bool recursive)
+        {
+            return new FileSystemEnumerable<PathInfo>(path, PathInfo.FromSystemEntry, recursive ? recursiveEnumerator : defaultEnumerator)
+            {
+                ShouldIncludePredicate = (ref FileSystemEntry entry) => FileSystemName.MatchesSimpleExpression(expression.AsSpan(), entry.FileName)
+            };
+        }
+
+        public struct PathInfo
+        {
+            public bool IsDirectory;
+            public string FullPath;
+
+            public static PathInfo FromSystemEntry(ref FileSystemEntry entry)
+            {
+                return new PathInfo
+                {
+                    IsDirectory = entry.IsDirectory,
+                    FullPath = entry.ToFullPath()
+                };
+            }
+        }
+
+        [GeneratedRegex("\\s\\d+$", RegexOptions.RightToLeft)]
+        private static partial Regex FileNameIndexRegex();
     }
 }
