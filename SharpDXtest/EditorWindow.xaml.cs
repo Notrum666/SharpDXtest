c6f0a07a32fdf5177a1adbef0f1f647f124514df
Yahima <yaroslav.r.boyev@gmail.com>
2024-01-02 04:23:17 +0300

    Setup Scene5 creation

--- SharpDXtest/EditorWindow.xaml.cs
+++ SharpDXtest/EditorWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Input;
 using System.Windows.Interop;
 
 using Engine;
+using Engine.BaseAssets.Components;
+using LinearAlgebra;
+using SharpDXtest.Assets.Components;
 
 namespace Editor
 {
@@ -64,8 +67,53 @@ namespace Editor
         private void CreateBaseScene()
         {
             Scene.CurrentScene = new Scene();
-            GameObject obj1 = GameObject.Instantiate("TestObject_1");
-            GameObject obj2 = GameObject.Instantiate("TestObject_2", obj1.Transform);
+            GameObject cameraObj = GameObject.Instantiate("Camera");
+            Transform cameraTransform = cameraObj.GetComponent<Transform>();
+            cameraTransform.Position = new Vector3(0, -40, 90);
+            cameraTransform.Rotation = Quaternion.FromEuler(new Vector3(-45));
+            Camera camera = cameraObj.AddComponent<Camera>();
+            camera.FOV = 16f / 9f;
+            camera.Aspect = 16f / 9f;
+            camera.Near = 0.001;
+            camera.Far = 500;
+            cameraObj.AddComponent<SoundListener>();
+            cameraObj.AddComponent<CameraController>().Speed = 5;
+            cameraObj.AddComponent<InspectorTestComponent>();
+
+            GameObject gasVolumeObj = GameObject.Instantiate("GasVolume");
+            Transform gasVolumeTransform = gasVolumeObj.GetComponent<Transform>();
+            gasVolumeTransform.Position = new Vector3(0, 0, 60);
+            gasVolumeObj.AddComponent<GasVolume>().Size = new Vector3f(200, 200, 50);
+
+            GameObject cubeObj1 = GameObject.Instantiate("Plane");
+            Transform cubeObj1Transform = cubeObj1.GetComponent<Transform>();
+            cubeObj1Transform.LocalScale = new Vector3(50, 50, 0.5);
+            MeshComponent cubeObj1Mesh = cubeObj1.AddComponent<MeshComponent>();
+            cubeObj1Mesh.Model = AssetsManager.LoadAssetAtPath<Model>("BaseAssets\\Models\\cube.obj");
+
+            GameObject cubeObj2 = GameObject.Instantiate("Cube");
+            Transform cubeObj2Transform = cubeObj2.GetComponent<Transform>();
+            cubeObj2Transform.Position = new Vector3(0, 0, 2);
+            cubeObj2Transform.LocalRotation = Quaternion.FromEuler(new Vector3(45, 45, 0));
+            cubeObj2Transform.LocalScale = new Vector3(1, 1, 2);
+            MeshComponent cubeObj2Mesh = cubeObj2.AddComponent<MeshComponent>();
+            cubeObj2Mesh.Model = AssetsManager.LoadAssetAtPath<Model>("BaseAssets\\Models\\cube_materials.fbx");
+
+            GameObject light1 = GameObject.Instantiate("Light1");
+            light1.GetComponent<Transform>().LocalPosition = new Vector3(5, 0, 3);
+            light1.AddComponent<PointLight>().Radius.Set(10);
+
+            GameObject light2 = GameObject.Instantiate("Light2");
+            light2.GetComponent<Transform>().LocalPosition = new Vector3(-5, 0, 3);
+            light2.AddComponent<PointLight>().Radius.Set(10);
+
+            GameObject light3 = GameObject.Instantiate("Light3");
+            light3.GetComponent<Transform>().LocalPosition = new Vector3(-5, 0, 3);
+            light3.AddComponent<PointLight>().Radius.Set(10);
+
+            GameObject light4 = GameObject.Instantiate("Light4");
+            light4.GetComponent<Transform>().LocalPosition = new Vector3(5, 0, 3);
+            light4.AddComponent<PointLight>().Radius.Set(10);
         }
     }
 }
