b12bf234ac4ddc63996bf46d6d5f12da80ca749f
Yaroslav Boyev <yaroslav.r.boyev@gmail.com>
2024-01-06 02:54:14 +0300

    Fix EditorWindow after merge

--- SharpDXtest/EditorWindow.xaml.cs
+++ SharpDXtest/EditorWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace Editor
         public const string EditorPathVarName = "EnvVar_SharpDxEditor";
         private const string DataFolderName = "SharpDxEditor";
         private const string ResourcesFolderName = "Resources";
-        
+
         public static string DataFolderPath { get; private set; }
         public static string EditorFolderPath { get; private set; }
         public static string ResourcesFolderPath { get; private set; }
@@ -58,8 +58,6 @@ namespace Editor
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var mainPath = Directory.GetCurrentDirectory();
-            var solutionPath = Directory.GetParent(mainPath)?.Parent?.Parent?.Parent?.Parent?.FullName;
             ProjectsManager.InitializeInFolder(DataFolderPath);
 
             OpenProjectBrowserDialog();
@@ -125,10 +123,26 @@ namespace Editor
             GameObject cubeObj2 = GameObject.Instantiate("Cube");
             Transform cubeObj2Transform = cubeObj2.GetComponent<Transform>();
             cubeObj2Transform.Position = new Vector3(0, 0, 2);
-            cubeObj2Transform.LocalRotation = Quaternion.FromEuler(new Vector3(45, 45, 0));
+            cubeObj2Transform.LocalRotation = Quaternion.FromEuler(new Vector3(45 * (3.14 / 180), 45 * (3.14 / 180), 0));
             cubeObj2Transform.LocalScale = new Vector3(1, 1, 2);
             MeshComponent cubeObj2Mesh = cubeObj2.AddComponent<MeshComponent>();
             cubeObj2Mesh.Model = AssetsManager.LoadAssetAtPath<Model>("Models\\cube_materials.fbx");
+
+            GameObject cesiumMan = GameObject.Instantiate("CesiumMan");
+            Transform cesiumManTransform = cesiumMan.GetComponent<Transform>();
+            cesiumManTransform.Position = new Vector3(0, 0, 5);
+            cesiumManTransform.Rotation = Quaternion.FromEuler(new Vector3(-90 * (3.14 / 180), 0, 0));
+            SkeletalMeshComponent cesiumManMesh = cesiumMan.AddComponent<SkeletalMeshComponent>();
+            cesiumManMesh.Model = AssetsManager.LoadAssetAtPath<Model>("Models\\cesium_man.fbx");
+            cesiumManMesh.Animation = AssetsManager.LoadAssetByGuid<SkeletalAnimation>(new Guid("32c68bd7597e4c1f9c1037607098c766"));
+
+            GameObject cesiumMan2 = GameObject.Instantiate("CesiumMan2");
+            Transform cesiumManTransform2 = cesiumMan2.GetComponent<Transform>();
+            cesiumManTransform2.Position = new Vector3(0, 2, 5);
+            cesiumManTransform2.Rotation = Quaternion.FromEuler(new Vector3(-90 * (3.14 / 180), 0, 0));
+            SkeletalMeshComponent cesiumManMesh2 = cesiumMan2.AddComponent<SkeletalMeshComponent>();
+            cesiumManMesh2.Model = AssetsManager.LoadAssetAtPath<Model>("Models\\cesium_man.fbx");
+            //cesiumManMesh2.AnimationIndex = 14;
 
             GameObject light1 = GameObject.Instantiate("Light1");
             light1.GetComponent<Transform>().LocalPosition = new Vector3(5, 0, 3);
