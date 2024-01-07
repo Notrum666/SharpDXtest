ff06f764a769ce4c222a0edd09c4cb0a3a5002bf
Notrum666 <30927048+Notrum666@users.noreply.github.com>
2024-01-07 09:22:18 +0700

    removed all vars and unnecessary curly brackets

--- SharpDXtest/GameProject/ProjectViewModel.cs
+++ SharpDXtest/GameProject/ProjectViewModel.cs
@@ -40,8 +40,6 @@ namespace Editor.GameProject
         {
             ProjectViewModel project = new ProjectViewModel(projectData.ProjectName, projectData.ProjectFolderPath);
 
-            // var mainPath = Directory.GetCurrentDirectory();
-            // var projectFolderPath = Directory.GetParent(mainPath)?.Parent?.Parent?.Parent?.Parent?.FullName;
             string projectFolderPath = project.FolderPath;
             AssetsManager.InitializeInFolder(projectFolderPath);
             AssetsRegistry.InitializeInFolder(projectFolderPath);
