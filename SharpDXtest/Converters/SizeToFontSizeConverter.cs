2cd7f059ac7f7395070dbdcfe7aeb8f6bf732010
Notrum666 <30927048+Notrum666@users.noreply.github.com>
2024-01-19 17:22:41 +0700

    binded fontsize in contentbrowser to items size

---
+++ SharpDXtest/Converters/SizeToFontSizeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Editor
+{
+    [ValueConversion(typeof(double), typeof(double))]
+    public sealed class SizeToFontSizeConverter : IValueConverter
+    {
+        public double Denominator { get; set; } = 1.0;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value / Denominator;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value * Denominator;
+        }
+    }
+}
