084ceffae6f26299b0787092f2008a67bcbb86aa
Yahima <yaroslav.r.boyev@gmail.com>
2024-01-21 11:23:50 +0300

    Add UserControlTest test

--- TestProject/Content/UserControlTest.xaml.cs
+++ TestProject/Content/UserControlTest.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,12 @@ namespace TestProject
         public UserControlTest()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Texture shapes = AssetsManager.LoadAssetAtPath<Texture>("shapes.png");
+            TestImage.Source = shapes.Source;
         }
     }
 }
