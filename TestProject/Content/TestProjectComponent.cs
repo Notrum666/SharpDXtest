d94511f420e966c5af306480ab99e5ad567294b5
Notrum666 <30927048+Notrum666@users.noreply.github.com>
2024-01-25 15:45:42 +0700

    added IgnoreGravity property to Rigidbody

--- TestProject/Content/TestProjectComponent.cs
+++ TestProject/Content/TestProjectComponent.cs
@@ -2,9 +2,11 @@ using Engine;
 using Engine.BaseAssets.Components;
 using System;
 using System.Collections;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,11 +14,36 @@ using System.Windows.Markup;
 
 namespace TestProject
 {
-    public class TestProjectComponent : BehaviourComponent
+    public class TestProjectComponent : BehaviourComponent, INotifyPropertyChanged
     {
+        private string text = "";
+        public string Text
+        {
+            get => text;
+            set
+            {
+                text = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
         public override void Start()
         {
-            GameObject.GetComponent<Rigidbody>().Material = new PhysicalMaterial(0.0, 1.0, CombineMode.Maximum, CombineMode.Maximum);
+            GraphicsCore.ViewportPanel.Dispatcher.Invoke(() =>
+            {
+                UserControlTest userControlTest = new UserControlTest();
+                GraphicsCore.ViewportPanel.Children.Add(userControlTest);
+                userControlTest.DataContext = this;
+            });
+        }
+        public override void Update()
+        {
+            Text = Time.DeltaTime.ToString();
         }
     }
 }
