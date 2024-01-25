951a0b20f4512cc44a931f076e8b7de430dad60b
Nikolay Kelchin <50986737+xbadmojox@users.noreply.github.com>
2024-01-25 21:33:55 +0300

    Create InferenceEngine.cs

---
+++ Engine/InferenceEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Documents;
+
+namespace Engine
+{
+    public static class InferenceEngine
+    {
+        static InferenceEngine()
+        {
+        }
+
+        public static ActionRefund GetAction(List<AgentKnowledge> knowledges)
+        {
+            return new ActionRefund();
+        }
+    }
+    public class AgentKnowledge
+    {
+        public string ObjectType { get; set; }
+        public string PropertyName { get; set; }
+        public object ObjectValue { get; set; }
+    }
+    public class ActionRefund
+    {
+        public string ObjectType { get; set; }
+        public string ActionName { get; set; }
+        public object ObjectValue { get; set; }
+    }
+}
